031895d09d58ca15e10a30d5732a5c1738b33f6a
harshtiwari04 <145828947+harshtiwari04@users.noreply.github.com>
2026-07-05 00:02:39 +0530

    Add: count-number-of-islands - submission-2

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-2.cpp
@@ -0,0 +1,50 @@
+class Solution {
+    private:
+    void bfs(int row,int col,vector<vector<int>>&vis,vector<vector<char>>& grid){
+        int n=grid.size();
+        int m=grid[0].size();
+
+        queue<pair<int,int>>q;
+        q.push({row,col});
+        vis[row][col]=1;
+
+        int dx[4]={1,0,-1,0};
+        int dy[4]={0,-1,0,1};
+
+        while(!q.empty()){
+            auto [r,c] = q.front();
+            q.pop();
+
+            for(int k=0;k<4;k++){
+                int newr=r+dx[k];
+                int newc=c+dy[k];
+
+
+                if(newr>=0 && newr<n && newc>=0 && newc<m
+                && !vis[newr][newc] && grid[newr][newc]=='1'){
+                    vis[newr][newc]=1;
+                    q.push({newr,newc});
+                }
+            }
+        }
+    }
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int n=grid.size();
+        int m = grid[0].size();
+
+        vector<vector<int>>vis(n,vector<int>(m,0));
+
+        int count =0;
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(!vis[i][j] && grid[i][j]=='1'){
+                    bfs(i,j,vis,grid);
+                    count++;
+                }
+            }
+        }
+    return count ;    
+    }
+};
